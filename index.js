7b25822e36c17a684e3bad7a41a324e7be6e1e9a
Matthew Friedberg <mfriedberg@netflix.com>
2021-08-28 15:15:03 -0700

    Set up the POST request with Axios
    create our POST request in our /callback route handler to send the authorization code back to the Spotify server.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const querystring = require("querystring");
+const axios = require("axios");
 const app = express();
 const port = 8888;
 const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env;
@@ -45,7 +46,33 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/callback", (req, res) => {
-  res.send("Callback");
+  const code = req.query.code || null;
+
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: REDIRECT_URI,
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+      } else {
+        res.send(response);
+      }
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 app.listen(port, () => {
