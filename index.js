8580aac87d81ba8d3359555be70243587b0bfeb9
Matthew Friedberg <mfriedberg@netflix.com>
2021-08-28 15:24:53 -0700

    Set up the /refresh_token route handler
    This route handler is almost the same as the /callback handler, except for a few small differences. The grant_type is refresh_token instead of authorization_code, and we're sending along a refresh_token instead of an authorization code.

--- index.js
+++ index.js
@@ -65,9 +65,9 @@ app.get("/callback", (req, res) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        const { access_token, token_type } = response.data;
+        const { access_token, token_type, refresh_token } = response.data;
 
-        axios
+        /* axios
           .get("https://api.spotify.com/v1/me", {
             headers: {
               Authorization: `${token_type} ${access_token}`,
@@ -78,10 +78,46 @@ app.get("/callback", (req, res) => {
           })
           .catch((error) => {
             res.send(error);
+          }); */
+
+        axios
+          .get(
+            `http://localhost:8888/refresh_token?refresh_token=${refresh_token}`
+          )
+          .then((response) => {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          })
+          .catch((error) => {
+            res.send(error);
           });
       } else {
         res.send(response);
       }
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+app.get("/refresh_token", (req, res) => {
+  const { refresh_token } = req.query;
+
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
+    },
+  })
+    .then((response) => {
+      res.send(response.data);
     })
     .catch((error) => {
       res.send(error);
