ce207d28a596a873fb0a79cd9d0b87e0222b6ec9
Matthew Friedberg <mfriedberg@netflix.com>
2021-08-28 15:00:31 -0700

    Add state and scope query params

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
-require('dotenv').config();
-const express = require('express');
-const querystring = require('querystring');
+require("dotenv").config();
+const express = require("express");
+const querystring = require("querystring");
 const app = express();
 const port = 8888;
 const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env;
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = "spotify_auth_state";
+
 // app.METHOD(PATH, HANDLER)
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World HELLO')
+app.get("/", (req, res) => {
+  res.send("Hello World HELLO");
 });
 
-app.get('/login', (req, res) => {
-    const queryParams = querystring.stringify({
-        client_id: CLIENT_ID,
-        response_type: 'code',
-        redirect_uri: REDIRECT_URI
-    })
-    res.redirect(`http://accounts.spotify.com/authorize?${queryParams}`);
-})
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = "user-read-private user-read-email";
+
+  const queryParams = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: "code",
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
 
 app.listen(port, () => {
-    console.log(`Express app listening at http://localhost:${port}`);
+  console.log(`Express app listening at http://localhost:${port}`);
 });
