eecef9897056f8f3e1835bd762919418035090cf
Matthew Friedberg <mfriedberg@netflix.com>
2021-08-28 14:55:56 -0700

    Updated to use queryparams

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const querystring = require('querystring');
 const app = express();
 const port = 8888;
 const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env;
@@ -11,8 +12,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    res.redirect(`http://accounts.spotify.com/authorize?
-    client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`)
+    const queryParams = querystring.stringify({
+        client_id: CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: REDIRECT_URI
+    })
+    res.redirect(`http://accounts.spotify.com/authorize?${queryParams}`);
 })
 
 app.listen(port, () => {
