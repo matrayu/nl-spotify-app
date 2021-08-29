8a553c489787e20f2144ebfe5995bca2a978e380
Matthew Friedberg <mfriedberg@netflix.com>
2021-08-28 19:28:45 -0700

    Added higher-order func to catch errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,22 @@
-import { useEffect, useState } from 'react';
-import { accessToken, logout, getCurrentUserProfile } from './spotify';
-import './App.css';
+import { useEffect, useState } from "react";
+import { accessToken, logout, getCurrentUserProfile } from "./spotify";
+import { catchErrors } from "./utils";
+import "./App.css";
 
 function App() {
   const [token, setToken] = useState(null);
   const [profile, setProfile] = useState(null);
-  
+
   useEffect(() => {
-    setToken(accessToken)
+    setToken(accessToken);
 
     const fetchData = async () => {
-      try {
-        const { data } = await getCurrentUserProfile();
-        setProfile(data);
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
 
-        console.log(data)
-
-      } catch (e) {
-        console.error(e);
-      }
-    }
-
-    fetchData();
-
+    catchErrors(fetchData());
+    
   }, []);
 
   return (
@@ -41,7 +35,7 @@ function App() {
                 <h1>{profile.display_name}</h1>
                 <p>{profile.followers.total} Followers</p>
                 {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar"/>
+                  <img src={profile.images[0].url} alt="Avatar" />
                 )}
               </div>
             )}
