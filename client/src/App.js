730c7afe66de0755868d31259a70237b7e4d8659
Matthew Friedberg <mfriedberg@netflix.com>
2021-08-28 19:33:53 -0700

    Added React Router and routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import { catchErrors } from "./utils";
 import "./App.css";
@@ -16,30 +17,47 @@ function App() {
     };
 
     catchErrors(fetchData());
-    
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        {!token ? (
+      {!token ? (
           <a className="App-link" href="http://localhost:8888/login">
             Log in to Spotify
           </a>
         ) : (
-          <>
-            <button onClick={logout}>Log Out</button>
+          <Router>
+            <Switch>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/playlists/:id">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/playlists">
+                <h1>Playlists</h1>
+              </Route>
+              <Route path="/">
+                <>
+                  <button onClick={logout}>Log Out</button>
 
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar" />
-                )}
-              </div>
-            )}
-          </>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )}
+                </>
+              </Route>
+            </Switch>
+          </Router>
         )}
       </header>
     </div>
