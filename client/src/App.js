e46245312709f0added57980f6d402cf4d0f7cc0
Matthew Friedberg <mfriedberg@netflix.com>
2021-08-28 19:24:59 -0700

    Updated App.js to display profile data

--- client/src/App.js
+++ client/src/App.js
@@ -34,8 +34,17 @@ function App() {
           </a>
         ) : (
           <>
-            <h1>Logged in!</h1>
             <button onClick={logout}>Log Out</button>
+
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Avatar"/>
+                )}
+              </div>
+            )}
           </>
         )}
       </header>
