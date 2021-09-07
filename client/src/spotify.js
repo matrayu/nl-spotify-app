b5e717e1ab64469c8c54eee455a1ba210548b341
Matthew Friedberg <mfriedberg@netflix.com>
2021-09-07 15:47:26 -0700

    add func to hit playlists endpoint

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -131,6 +131,15 @@ const getAccessToken = () => {
 export const accessToken = getAccessToken();
 
 /**
+ * Get a List of Current User's Playlists
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-a-list-of-current-users-playlists
+ * @returns {Promise}
+ */
+ export const getCurrentUserPlaylists = (limit = 20) => {
+  return axios.get(`/me/playlists?limit=${limit}`);
+};
+
+/**
  * Axios global request headers
  * https://github.com/axios/axios#global-axios-defaults
  */
