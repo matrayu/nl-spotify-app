a0cd3a06d6c22324401f2c18b417bb08f4da21c1
Matthew Friedberg <mfriedberg@netflix.com>
2021-09-07 16:01:42 -0700

    Update Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -21,25 +21,34 @@ const Profile = () => {
 
   return (
     <>
-      <StyledHeader type="user">
-            <div className="header__inner">
-              {profile.images.length && profile.images[0].url && (
-                <img className="header__img" src={profile.images[0].url} alt="Avatar"/>
-              )}
-              <div>
-                <div className="header__overline">Profile</div>
-                <h1 className="header__name">{profile.display_name}</h1>
-                <p className="header__meta">
-                  {playlists && (
-                    <span>{playlists.total} Playlist{playlists.total !== 1 ? 's' : ''}</span>
-                  )}
+      {profile && (
+        <StyledHeader type="user">
+          <div className="header__inner">
+            {profile.images.length && profile.images[0].url && (
+              <img
+                className="header__img"
+                src={profile.images[0].url}
+                alt="Avatar"
+              />
+            )}
+            <div>
+              <div className="header__overline">Profile</div>
+              <h1 className="header__name">{profile.display_name}</h1>
+              <p className="header__meta">
+                {playlists && (
                   <span>
-                    {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ''}
+                    {playlists.total} Playlist{playlists.total !== 1 ? "s" : ""}
                   </span>
-                </p>
-              </div>
+                )}
+                <span>
+                  {profile.followers.total} Follower
+                  {profile.followers.total !== 1 ? "s" : ""}
+                </span>
+              </p>
             </div>
-          </StyledHeader>
+          </div>
+        </StyledHeader>
+      )}
     </>
   );
 };
