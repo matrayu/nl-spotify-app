bf2f0a96825a95a56bab624d23389d64cd489aa1
Matthew Friedberg <mfriedberg@netflix.com>
2021-09-07 15:32:04 -0700

    Created StyledHeader component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,32 +1,35 @@
-import { useState, useEffect } from 'react';
-import { catchErrors } from '../utils';
-import { getCurrentUserProfile } from '../spotify';
+import { useState, useEffect } from "react";
+import { catchErrors } from "../utils";
+import { getCurrentUserProfile } from "../spotify";
+import { StyledHeader } from "../styles";
 
 const Profile = () => {
-    const [profile, setProfile] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const { data } = await getCurrentUserProfile(); 
-            setProfile(data);
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
 
-        catchErrors(fetchData())
-    }, []);
+    catchErrors(fetchData());
+  }, []);
 
-    return (
-        <>
-            {profile && (
-                <div>
-                    <h1>{profile.display_name}</h1>
-                    <p>{profile.followers.total} Followers</p>
-                    {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="Avatar"/>
-                    )}
-                </div>
+  return (
+    <>
+      <StyledHeader type="user">
+        {profile && (
+          <div>
+            <h1>{profile.display_name}</h1>
+            <p>{profile.followers.total} Followers</p>
+            {profile.images.length && profile.images[0].url && (
+              <img src={profile.images[0].url} alt="Avatar" />
             )}
-        </>
-    )
-}
+          </div>
+        )}
+      </StyledHeader>
+    </>
+  );
+};
 
 export default Profile;
