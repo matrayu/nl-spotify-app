ea4fba2eb77aeed9d53c8b0dcfa870b5b76d82bb
Matthew Friedberg <mfriedberg@netflix.com>
2021-09-07 15:13:39 -0700

    Updated app to use code found in tut

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   useLocation,
-} from "react-router-dom";
-import { accessToken, logout, getCurrentUserProfile } from "./spotify";
-import { catchErrors } from "./utils";
-import { GlobalStyle } from "./styles";
-import { Login, Profile } from "./pages";
+} from 'react-router-dom';
+import { accessToken, logout } from './spotify';
+import { Login, Profile } from './pages';
+import { GlobalStyle } from './styles';
 import styled from 'styled-components/macro';
 
 const StyledLogoutButton = styled.button`
@@ -42,39 +41,43 @@ function ScrollToTop() {
 function App() {
   const [token, setToken] = useState(null);
 
-  return (
-    <div className="App">
-      <GlobalStyle />
-      <header className="App-header">
-        {!token ? (
-          <Login />
-        ) : (
-          <>
-            <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+  useEffect(() => {
+    setToken(accessToken);
+  }, []);
 
-            <Router>
-              <ScrollToTop />
-              <Switch>
-                <Route path="/top-artists">
-                  <h1>Top Artists</h1>
-                </Route>
-                <Route path="/top-tracks">
-                  <h1>Top Tracks</h1>
-                </Route>
-                <Route path="/playlists/:id">
-                  <h1>Playlist</h1>
-                </Route>
-                <Route path="/playlists">
-                  <h1>Playlists</h1>
-                </Route>
-                <Route path="/">
-                  <Profile />
-                </Route>
-              </Switch>
-            </Router>
-          </>
-        )}
-      </header>
+  return (
+    <div className="app">
+      <GlobalStyle />
+
+      {!token ? (
+        <Login />
+      ) : (
+        <>
+          <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+
+          <Router>
+            <ScrollToTop />
+
+            <Switch>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/playlists/:id">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/playlists">
+                <h1>Playlists</h1>
+              </Route>
+              <Route path="/">
+                <Profile />
+              </Route>
+            </Switch>
+          </Router>
+        </>
+      )}
     </div>
   );
 }
